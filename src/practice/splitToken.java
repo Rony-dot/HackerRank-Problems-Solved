bb83b31f6ccdc4881a41570024f0063779ac444e
Rakibul Hassan Rony <2018000000012@seu.edu.bd>
2021-07-08 11:23:33 +0600

    initialize

---
+++ src/practice/splitToken.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class splitToken {
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        System.out.println(min);
+        System.out.println(max);
+       // sc.close();
+    }
+}
